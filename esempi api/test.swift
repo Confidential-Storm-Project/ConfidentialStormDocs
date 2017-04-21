af583c22abac92570047c4f3264ffa66580d68f8
Nicholas Giordano <nicholas.giordano@live.com>
2017-04-21 18:51:31 +0200

    esempio creazione user ios
    per @marius93rm

---
+++ esempi api/test.swift
@@ -0,0 +1,31 @@
+//esempio che crea un nuovo utente con nik cio, password caca e mail d@f.t
+import Foundation
+
+let headers = [
+  "content-type": "application/x-www-form-urlencoded",
+  "cache-control": "no-cache",
+  "postman-token": "8bd801fc-7b9a-8aaa-772c-813a9df60682"
+]
+
+let postData = NSMutableData(data: "user=cio".data(using: String.Encoding.utf8)!)
+postData.append("&password=cacca".data(using: String.Encoding.utf8)!)
+postData.append("&mail=d@f.t".data(using: String.Encoding.utf8)!)
+
+let request = NSMutableURLRequest(url: NSURL(string: "http://nicholasgiordano.it:3005/user/register")! as URL,
+                                        cachePolicy: .useProtocolCachePolicy,
+                                    timeoutInterval: 10.0)
+request.httpMethod = "POST"
+request.allHTTPHeaderFields = headers
+request.httpBody = postData as Data
+
+let session = URLSession.shared
+let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+  if (error != nil) {
+    print(error)
+  } else {
+    let httpResponse = response as? HTTPURLResponse
+    print(httpResponse)
+  }
+})
+
+dataTask.resume()
