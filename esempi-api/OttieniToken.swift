a00323d682e2f146814088274fb1bbdb1323a76a
Nicholas Giordano <nicholas.giordano@live.com>
2017-04-21 22:04:01 +0200

    Esempio richiesta token per utente registrato

---
+++ esempi-api/OttieniToken.swift
@@ -0,0 +1,30 @@
+//ottenimento token per utente con user prova e password testpswd
+import Foundation
+
+let headers = [
+  "content-type": "prova",
+  "pswd": "testpswd",
+  "cache-control": "no-cache",
+  "postman-token": "7145b800-1363-a29e-5fbd-eb8394413cbe"
+]
+
+let postData = NSMutableData(data: "user=prova".data(using: String.Encoding.utf8)!)
+postData.append("&password=testpswd".data(using: String.Encoding.utf8)!)
+postData.append("&mail=test@test.it".data(using: String.Encoding.utf8)!)
+
+let request = NSMutableURLRequest(url: NSURL(string: "http://nicholasgiordano.it:3005/user/token")! as URL,
+                                        cachePolicy: .useProtocolCachePolicy,
+                                    timeoutInterval: 10.0)
+request.httpMethod = "GET"
+request.allHTTPHeaderFields = headers
+request.httpBody = postData as Data
+
+let session = URLSession.shared
+let dataTask = session.dataTask(with: request as URLRequest, completionHandler: { (data, response, error) -> Void in
+  if (error != nil) {
+    print(error)
+  } else {
+    let httpResponse = response as? HTTPURLResponse
+    print(httpResponse)
+  }
+})
